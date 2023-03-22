f154481ef89791b7f4c4fce61a6a156038b67e73
Bernard Paques <b.paques@reply.com>
2023-03-22 07:34:33 +0100

    introduce basic health check of the runtime

---
+++ tests/test_lambda_check_health_handler.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Copyright Reply.com or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import logging
+logging.getLogger('botocore').setLevel(logging.CRITICAL)
+logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+
+import os
+import pytest
+from unittest.mock import patch
+
+from lambdas.check_health_handler import handle_event
+
+pytestmark = pytest.mark.wip
+
+
+@pytest.mark.integration_tests
+@patch.dict(os.environ, dict(VERBOSITY='DEBUG'))
+def test_handle_event(monkeypatch):
+
+    result = handle_event(event=None, context=None)
+    assert result == '[OK]'
